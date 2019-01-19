ac359944c9d69c6aace7633bd6e5c30da415dfe8
RANJITHAGANGADHAR <ranjithagangadhar1997@gmail.com>
2019-01-19 14:52:36 +0530

    first commit

---
+++ file.js
@@ -0,0 +1,32 @@
+
+<html>
+	<head>
+		<title>JavaScript</title>
+	</head>
+	<body>
+	
+	<h1>Heading</h1>
+	
+	<script type="text/javascript">
+		
+		//
+		/*				*/
+		//	+	concatenation
+		
+		document.write("Hai Hello");
+		
+	</script>
+	
+	
+	<select>
+		<script>
+			for(i=1900;i<=2017;i++)
+			{
+				document.write("<option>"+i+"</option>");
+			}
+		</script>
+	</select>
+	
+	
+	</body>
+</html>
---
+++ first2.java
@@ -0,0 +1,54 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body link="green" alink="blue" vlink="red">
+	
+		<!--
+		
+		Anchor		<a></a>
+		
+		Pair, Inline
+		
+		href (mandatory)
+		
+		title
+		target=_self, _blank, _parent
+		accesskey
+		tabindex
+		name
+
+		-->
+		
+		<a href="https://www.facebook.com/" accesskey="g" target="_blank">Click Here for Facebook</a>
+		
+		<br/>
+		
+		<a href="https://www.facebook.com/" accesskey="g" target="_blank"> <img src="Image/social.png" /></a>
+		
+		<br/>
+		
+		<a href="basic.html" title="Hai">Click Here Internal</a>
+		
+		<br/>
+		
+		<a href="#xyz" name="abc" tabindex="1">Clik Here to Go to Bottom</a>
+		
+		<p>This is Paragraph</p>
+		
+		<a href="#abc" name="xyz">Clik Here to Go to Top</a>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	</body>
+</html>
